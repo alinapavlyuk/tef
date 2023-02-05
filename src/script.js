517d6e105adbaab4baffcd122310f4d8b4dd6155
alinapavlyuk <alino4kap12@gmail.com>
2023-02-06 03:11:16 +0900

    Added side navigation for smaller screen sizes

--- src/script.js
+++ src/script.js
@@ -1,86 +1,51 @@
-function showNavigationBar() {
-    let navigationListItems =
-        [
-            {
-                name: "Про кафедру",
-                link: "#",
-                dropdown: [
-                    {name: "Історія", link: "#"},
-                    {name: "Положення", link: "#"},
-                    {name: "Матеріали акредитації", link: "#"},
-                    {name: "Викладачі", link: "#"},
-                    {name: "Партнери",link: "#"},
-                    {name: "Міжнародна співпраця", link: "#"},
-                    {name: "Випускники", link: "#"},
-                    {name: "Документообіг",link: "#"},
-                    {name: "Дистанційні засоби навчання", link: "#"}
-                ]
-            },
-            {
-                name: "Вступ",
-                link: "#",
-                dropdown: [
-                    {name:"Про спеціальність", link: "#"},
-                    {name: "На 1 курс(за ЗНО)", link: "#"},
-                    {name: "Молодший спеціаліст", link: "#"},
-                    {name: "Магістратура", link: "#"},
-                    {name: "Аспірантура", link: "#"},
-                    {name: "Заочна форма навчання", link: "#"},
-                    {name: "Підготовчі курси", link: "#"}
-                ]
-            },
-            {
-                name: "Студенту",
-                link: "#",
-                dropdown: [
-                    {name: "Бакалавр", link: "#"},
-                    {name: "Магістр", link: "#"},
-                    {name: "Доктор філософії", link: "#"},
-                    {name: "Працевлаштування", link: "#"},
-                    {name: "Дистанційні ресурси для наукової роботи", link: "#"},
-                    {name: "Студрада", link: "#"},
-                    {name: "Гуртожитки",link: "#"},
-                    {name: "Дозвілля", link: "#"}
-                ]
-            },
-            {
-                name: "Наука",
-                link: "#",
-                dropdown: [
-                    {name: "Наукова робота зі студентами", link: "#"},
-                    {name: "Напрямки досліджень", link: "#"},
-                    {name: "Наукові школи", link: "#"},
-                    {name: "Наукові заходи", link: "#"}]
-            },
-            {
-                name: "Новини",
-                link: "#",
-                dropdown: []
-            },
-            {
-                name: "Контакти",
-                link: "#",
-                dropdown: []
-            }
-        ];
-    let navigationList = document.querySelector("#navigation-list");
+import {navigationListItems} from "./navigationListItems.js";
+
+function showMainNavigationBar() {
+    let navigationList = document.querySelector("#main-navigation-list");
     navigationListItems.forEach(function (item, index) {
         navigationList.innerHTML += `
-        <li id="nav-item-${index}">
-           <a href = ${item.link} id="link-${index}">${item.name}</a>
+        <li id="main-nav-item-${index}">
+           <a href = ${item.link} id="main-link-${index}">${item.name}</a>
            <div class="space-before-dropdown"></div>
         </li>
         `
         if(item.dropdown.length !== 0) {
-            let navigationListItem = document.querySelector(`#nav-item-${index}`);
+            let navigationListItem = document.querySelector(`#main-nav-item-${index}`);
             navigationListItem.classList.add("dropdown");
             navigationListItem.innerHTML += `
-                <div class="dropdown-content" id="dropdown-${index}"></div>
+                <div class="dropdown-content" id="main-dropdown-${index}"></div>
             `
-            let dropdownMenu = document.querySelector(`#dropdown-${index}`);
+            let dropdownMenu = document.querySelector(`#main-dropdown-${index}`);
             item.dropdown.forEach(function (dropdownItem) {
                 dropdownMenu.innerHTML += `
                 <a href = ${dropdownItem.link}>${dropdownItem.name}</a>
+                `
+            })
+        }
+    })
+}
+
+function showSideNavigationBar() {
+    let navigationList = document.querySelector("#offcanvas-navigation-list");
+    navigationListItems.forEach(function (item, index) {
+        navigationList.innerHTML += `
+        <li class="nav-item" id="side-nav-item-${index}">
+           <a href=${item.link} class="nav-link" id="side-link-${index}">${item.name}</a>
+        </li>
+        `
+        if(item.dropdown.length !== 0) {
+            let navigationLink = document.querySelector(`#side-link-${index}`);
+            navigationLink.classList.add("dropdown-toggle");
+            navigationLink.setAttribute("data-bs-toggle", "dropdown")
+            let navigationListItem = document.querySelector(`#side-nav-item-${index}`);
+            navigationListItem.classList.add("dropdown");
+            navigationListItem.innerHTML += `
+                <ul class="dropdown-menu" id="side-dropdown-${index}"></ul>
+            `
+            let dropdownMenu = document.querySelector(`#side-dropdown-${index}`);
+            item.dropdown.forEach(function (dropdownItem) {
+                dropdownMenu.innerHTML += `
+                <li><a class="dropdown-item" href="${dropdownItem.link}">${dropdownItem.name}</a></li>
                 `
             })
         }
@@ -101,7 +66,8 @@ function playAccordion() {
     }, 5000);
 }
 
-showNavigationBar();
+showMainNavigationBar();
+showSideNavigationBar();
 playAccordion();
 
 
