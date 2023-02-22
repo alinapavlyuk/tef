2925e97901d1ddb461b0ac44c69ab24d16cbd2c7
alinapavlyuk <alino4kap12@gmail.com>
2023-02-23 04:36:13 +0900

    Try to fix bug on iPhone

--- src/script.js
+++ src/script.js
@@ -91,3 +91,11 @@ showMainNavigationBar();
 showSideNavigationBar();
 playAccordion();
 splide.mount();
+
+const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+    console.log(window.innerHeight)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
