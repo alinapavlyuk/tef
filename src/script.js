733a0e84479452be437beb7c22ab6b4402e189c1
alinapavlyuk <alino4kap12@gmail.com>
2023-02-24 00:08:39 +0900

    Try to fix bug on iPhone 2

--- src/script.js
+++ src/script.js
@@ -92,10 +92,10 @@ showSideNavigationBar();
 playAccordion();
 splide.mount();
 
-const appHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-    console.log(window.innerHeight)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
+// const appHeight = () => {
+//     const doc = document.documentElement
+//     doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+//     console.log(window.innerHeight)
+// }
+// window.addEventListener('resize', appHeight)
+// appHeight()
