b2a04b5d03c74b6d656e6a4cd26dc3261de892e5
alinapavlyuk <alino4kap12@gmail.com>
2023-04-29 22:52:57 +0900

    Made accordion clickable

--- src/constructors/accordion.js
+++ src/constructors/accordion.js
@@ -1,15 +1,38 @@
-function playAccordion() {
-    let currentItemNum = 1;
-    let nextItemNum = 2;
+let timer;
+let currentItemNum = 1;
+let nextItemNum = 2;
 
-    setInterval(function () {
-        var activeItem = document.querySelector(`#accordion-item-${currentItemNum}`);
+function playAccordion() {
+    timer = setInterval(function () {
+        let activeItem = document.querySelector(`#accordion-item-${currentItemNum}`);
         activeItem.classList.remove("accordion-item-active");
-        var nextItem = document.querySelector(`#accordion-item-${nextItemNum}`);
+        let nextItem = document.querySelector(`#accordion-item-${nextItemNum}`);
         nextItem.classList.add("accordion-item-active");
         currentItemNum = currentItemNum % 4 + 1;
         nextItemNum = nextItemNum % 4 + 1;
-    }, 5000);
+    }, 4000);
+}
+
+function setNewSlide(n) {
+    clearInterval(timer);
+    let activeItem = document.querySelector(`#accordion-item-${currentItemNum}`);
+    activeItem.classList.remove("accordion-item-active");
+    currentItemNum = n;
+    nextItemNum = n % 4 + 1;
+    let newActiveItem = document.querySelector(`#accordion-item-${currentItemNum}`);
+    newActiveItem.classList.add("accordion-item-active");
+    playAccordion(n);
+}
+
+function handleClick() {
+    document.querySelectorAll(".photo-accordion-item").forEach((item)=>{
+        let number = item.id.charAt(item.id.length - 1);
+        console.log(number);
+        item.addEventListener("click", () => {
+            setNewSlide(number);
+        });
+    })
 }
 
 playAccordion();
+handleClick();
